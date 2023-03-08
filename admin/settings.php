48967d967e6120c1b8402d8fb54b2eeba1857345
Jaydee Ballaho <jaydee.ballaho@wmsu.edu.ph>
2023-03-08 22:31:32 +0800

    update settings page

--- admin/settings.php
+++ admin/settings.php
@@ -20,145 +20,54 @@
 ?>
 <body>
     <!-- Header -->
-    <header class="navbar navbar-expand-md navbar-dark sticky-top background-color-green-visible shadow-sm py-0">
-        <div class="container-fluid p-0 pe-3 pe-md-0">
-            <a class="navbar-brand col-md-3 col-lg-3 col-xl-2 me-0 px-3 py-3 background-color-green" href="../home/">
-                <img class="logo-icon" src="../img/greenscreen_logo.png" alt="">
-                <span class="ms-2 logo-name">GreenScreen</span>
-            </a>
-            <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarCollapse"></div>
-        </div>
-    </header>
+    <?php
+        require_once '../includes/topnav_admin.php';
+    ?>
     <div class="container-fluid">
         <div class="row">
             <?php
                 require_once '../includes/sidebar.php';
             ?>
             <main class="col-md-9 ms-sm-auto col-lg-9 col-xl-10 p-md-4">
-                <div class="row">
-                    <div class="col-12 mt-3 mt-md-0 d-md-none">
-                        <h5 class="card-title m-0">Settings</h5>
-                    </div>
-                    <div class="col-lg-5 col-xl-4 profile-menu">
-                        <div class="card profile school-year mt-3 mt-md-0 card-hover">
-                            <div class="card-body">
-                                <h6 class="card-title m-0 fw-bold">School Year</h6>
+                <div class="w-100">
+                    <h5 class="col-12 fw-bold mb-3 mt-3 mt-md-0">School Year Settings</h5>
+                    <form class="needs-validation" action="" method="post" id="form-settings">
+                        <div class="row g-2">
+                            <div class="col-12 col-md-4">
+                                <label for="activeschoolyear" class="form-label">Active School Year<span class="text-muted"></span></label>
+                                <input type="text" disabled="true" class="form-control" value="2023-2024" id="activeschoolyear" placeholder="" name="activeschoolyear">
+                            </div>
+                            <div class="col-12 col-md-4">
+                                <label for="startschoolyear" class="form-label">Start of School Year<span class="text-muted"></span></label>
+                                <input type="number" class="form-control" id="startschoolyear" placeholder="" name="startschoolyear" required="">
+                            </div>
+                            <div class="col-12 col-md-4">
+                                <label for="endschoolyear" class="form-label">End of School Year<span class="text-muted"></span></label>
+                                <input type="number" class="form-control" id="endschoolyear" placeholder="" name="endschoolyear" required="">
+                            </div>
+                            <h5 class="col-12 fw-bold mt-4 mb-1">Admission Period Settings</h5>
+                            <div class="col-12 col-md-4">
+                                <label for="currentadmissionperiod" class="form-label">Current Admission Period<span class="text-muted"></span></label>
+                                <input type="text" disabled="true" class="form-control" value="Jul 01, 2023 - Aug 01, 2023" id="currentadmissionperiod" placeholder="" name="currentadmissionperiod">
+                            </div>
+                            <div class="col-12 col-md-4">
+                                <label for="startdate" class="form-label">Start Date<span class="text-muted"></span></label>
+                                <input type="date" class="form-control" id="startdate" placeholder="" name="startdate" required="">
+                            </div>
+                            <div class="col-12 col-md-4">
+                                <label for="enddate" class="form-label">End Date<span class="text-muted"></span></label>
+                                <input type="date" class="form-control" id="enddate" placeholder="" name="enddate" required="">
+                            </div>
+                            <h5 class="col-12 fw-bold mt-4 mb-1">Automation Settings</h5>
+                            <div class="form-check form-switch col-12 col-md-12">
+                                <input class="form-check-input form-control" type="checkbox" id="automateddecision" name="automateddecision" checked>
+                                <label class="form-check-label form-label" for="automateddecision">Use Automated Admission Decision</label>
+                            </div>
+                            <div class="col-12 pt-2 pt-xl-3 mb-3 mb-xl-0 mt-xl-auto">
+                                <input class="btn btn-lg btn-success background-color-green btn-font apply-settings" type="submit" value="Apply New Settings" name="login">
                             </div>
                         </div>
-                        <div class="card profile admission-period mt-3">
-                            <div class="card-body">
-                                <h6 class="card-title fw-bold m-0">Admission Period</h6>
-                            </div>
-                        </div>
-                        <div class="card profile automation mt-3">
-                            <div class="card-body">
-                                <h6 class="card-title fw-bold m-0">Automation</h6>
-                            </div>
-                        </div>
-                    </div>
-                    <div id='school-year' class="col-lg-7 col-xl-8 profile-screen d-done d-lg-block ps-lg-1">
-                        <div class="mt-3 mt-md-0">
-                            <div class="card-profile p-4">
-                                <div class="row">
-                                    <h5 class="col-12 fw-bold mb-3">School Year Settings</h5>
-                                    <form class="needs-validation" action="" method="post">
-                                        <div class="row g-2">
-                                            <div class="col-12 col-xl-4">
-                                                <label for="activeschoolyear" class="form-label">Active School Year<span class="text-muted"></span></label>
-                                                <input type="text" disabled="true" class="form-control" value="2023-2024" id="activeschoolyear" placeholder="" name="activeschoolyear">
-                                            </div>
-                                            <div class="col-12 col-xl-4">
-                                                <label for="schoolyear" class="form-label">Set New School Year<span class="text-muted"></span></label>
-                                                <input type="text" class="form-control" id="schoolyear" placeholder="" name="schoolyear" required="">
-                                            </div>
-                                            <div class="col-12 col-xl-4 pt-2 pt-xl-3 mb-3 mb-xl-0 mt-xl-auto">
-                                                <input class="btn btn-lg btn-success background-color-green btn-font w-100" type="submit" value="Save Changes" name="login">
-                                            </div>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                            <div class="card-profile mt-3 p-4">
-                                <div class="w-100">
-                                    <h5 class="col-12 fw-bold mb-3">Past School Year</h5>
-                                    <div class="table-responsive">
-                                        <table class="table table-hover col-12">
-                                            <thead>
-                                                <tr>
-                                                    <th scope="col">#</th>
-                                                    <th scope="col">School Year</th>
-                                                    <th scope="col"></th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                <tr>
-                                                    <th scope="row">1</th>
-                                                    <td>2020-2021</td>
-                                                    <td align="right">
-                                                        <a href="" class="me-2 yellow"><i class="fa-solid fa-pen-to-square"></i></a>
-                                                        <a href="" class="red"><i class="fa-solid fa-trash-can"></i></a>
-                                                    </td>
-                                                </tr>
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div id='admission-period' class="col-lg-7 col-xl-8 profile-screen d-done d-lg-none ps-lg-1">
-                        <div class="mt-3 mt-md-0">
-                            <div class="card-profile p-4">
-                                <div class="row">
-                                    <h5 class="col-12 fw-bold mb-3">Period of Application</h5>
-                                    <form class="needs-validation" action="" method="post">
-                                        <div class="row g-2">
-                                            <div class="col-12 col-xl-4">
-                                                <label for="activeschoolyear" class="form-label">Active School Year<span class="text-muted"></span></label>
-                                                <input type="text" disabled="true" class="form-control" value="2023-2024" id="activeschoolyear" placeholder="" name="activeschoolyear">
-                                            </div>
-                                            <div class="col-12 col-xl-4">
-                                                <label for="schoolyear" class="form-label">Set New School Year<span class="text-muted"></span></label>
-                                                <input type="text" class="form-control" id="schoolyear" placeholder="" name="schoolyear" required="">
-                                            </div>
-                                            <div class="col-12 col-xl-4 pt-2 pt-xl-3 mb-3 mb-xl-0 mt-xl-auto">
-                                                <input class="btn btn-lg btn-success background-color-green btn-font w-100" type="submit" value="Save Changes" name="login">
-                                            </div>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                            <div class="card-profile mt-3 p-4">
-                                <div class="w-100">
-                                    <h5 class="col-12 fw-bold mb-3">Past School Year</h5>
-                                    <div class="col-12">
-                                        <table class="table table-hover col-12">
-                                            <thead>
-                                                <tr>
-                                                    <th scope="col">#</th>
-                                                    <th scope="col">School Year</th>
-                                                    <th scope="col"></th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                <tr>
-                                                    <th scope="row">1</th>
-                                                    <td>2020-2021</td>
-                                                    <td align="right">
-                                                        <a href="" class="me-2 yellow"><i class="fa-solid fa-pen-to-square"></i></a>
-                                                        <a href="" class="red"><i class="fa-solid fa-trash-can"></i></a>
-                                                    </td>
-                                                </tr>
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    </form>
                 </div>
             </main>
         </div>
@@ -166,28 +75,6 @@
 </body>
     <script>
         $(document).ready(function(){
-            $('.card.profile').on('click', function(){
-                $('.card.profile').removeClass('card-hover');
-                $(this).addClass('card-hover');
-            });
-            $('.card.profile.school-year').on('click', function(){
-                $('.profile-screen').hide();
-                $('.profile-screen').removeClass('d-lg-block');
-                $('.profile-screen').removeClass('d-lg-none');
-                $('#school-year').addClass('d-lg-block');
-            });
-            $('.card.profile.admission-period').on('click', function(){
-                $('.profile-screen').hide();
-                $('.profile-screen').removeClass('d-lg-block');
-                $('.profile-screen').removeClass('d-lg-none');
-                $('#admission-period').addClass('d-lg-block');
-            });
-            $('.card.profile.automation').on('click', function(){
-                $('.profile-screen').hide();
-                $('.profile-screen').removeClass('d-lg-block');
-                $('.profile-screen').removeClass('d-lg-none');
-                $('#automation').addClass('d-lg-block');
-            });
         });
     </script>
 </html>
